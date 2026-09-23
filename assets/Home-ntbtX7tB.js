e5e6b79aae576d12989e06ef36b945c4bc7e55c9
uedarap <uedarap@gmail.com>
2026-09-23 18:09:06 -0300

    feat: Update site to maintenance mode with new MaintenancePage component
    - Changed index.html and dist/index.html to reflect site under maintenance.
    - Updated comming-soon/index.html with new asset links.
    - Created MaintenancePage.vue component for displaying maintenance message.
    - Refactored CommingSoon.vue and Home.vue to use MaintenancePage component.
    - Updated meta tags for SEO to indicate site is under maintenance.

---
+++ assets/Home-ntbtX7tB.js
@@ -0,0 +1,1 @@
+import{a as e,o as a,M as o}from"./router-B1Dd8HfR.js";const s={__name:"Home",setup(t){return(c,n)=>(a(),e(o))}};export{s as default};
---
+++ assets/comming_soon-gkmEbCOh.js
@@ -0,0 +1,1 @@
+import{c as o,r as m}from"./router-B1Dd8HfR.js";import r from"./CommingSoon-CagCcx5w.js";o(r).use(m).mount("#comming-soon");
